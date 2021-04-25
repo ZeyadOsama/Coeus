c7505fd882e1829b3292c51fbea1ef1345b48953
Mohamed Alharouni <mohamedharouni1@gmail.com>
2021-04-25 22:03:24 +0200

    NFA stated skeleton added

---
+++ include/coeus/states/NFAstate.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Mohamed Harouni on 25/04/2021.
+//
+
+#ifndef COEUS_NFASTATE_H
+#define COEUS_NFASTATE_H
+
+namespace coeus {
+    namespace states {
+        class NFAstate {
+
+        };
+    } // namespace states
+}// namespace coeus
+
+#endif //COEUS_NFASTATE_H
---
+++ src/states/NFAstate.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Mohamed Harouni on 25/04/2021.
+//
+
+#include <coeus/states/NFAstate.hpp>
+
